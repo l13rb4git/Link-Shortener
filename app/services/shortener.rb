1da45d7544530849c4ff2d20035f57148f70afc0
macunaima <gcgamer46@gmail.com>
2020-10-05 14:46:55 -0300

    Following the dependency injection pratices

--- app/services/shortener.rb
+++ app/services/shortener.rb
@@ -2,9 +2,10 @@ require 'digest/sha2'
 
 
 class Shortener
-  attr_reader :url
-  def initialize(url)
+  attr_reader :url, :link_model
+  def initialize(url, link_model: Link)
     @url = url
+    @link_model = link_model
   end
 
   def lookup_code
@@ -12,6 +13,6 @@ class Shortener
   end
 
   def generate_short_link
-    Link.new(original_url: url, lookup_code: lookup_code)
+    link_model.new(original_url: url, lookup_code: lookup_code)
   end
 end
