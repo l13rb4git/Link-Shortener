076689610454513a39441ebd66bab951e19a0a20
macunaima <gcgamer46@gmail.com>
2020-10-05 14:02:57 -0300

    testing if the link can be saved

--- spec/models/link_spec.rb
+++ spec/models/link_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe Link, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  it 'is valid if it has an original URL and a lookup code' do
+    link = Link.new(
+      original_url: "https://www.website.com/videos/first-video.mp4",
+      lookup_code: "1234567"
+    )
+
+    expect(link.valid?).to be(true)
+  end
 end
