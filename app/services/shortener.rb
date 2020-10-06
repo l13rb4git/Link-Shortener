df542f4aea1643cf705ee29d68ae984f8db6624f
macunaima <gcgamer46@gmail.com>
2020-10-06 11:12:51 -0300

    Returning to the SHA256 method, now, using a count to make sure that the input never string repeat

--- app/services/shortener.rb
+++ app/services/shortener.rb
@@ -1,8 +1,8 @@
-require 'securerandom'
+require 'digest/sha2'
 
 
 class Shortener
-  attr_reader :url, :link_model
+  attr_reader :url, :link_model, :link_count
 
   def initialize(url, link_model: Link)
     @url = url
@@ -10,6 +10,7 @@ class Shortener
   end
 
   # TODO: Delete this method if it inst in use #
+  #       by now, it is used only for testing 
   def lookup_code
     loop do
       code = generate_code
@@ -24,7 +25,15 @@ class Shortener
   private 
 
   def generate_code
-    SecureRandom.base64(12)[0..6]
+    Digest::SHA256.hexdigest(unique_id)[0..6]
+  end
+
+  def unique_id
+    "#{link_count}#{url}"
+  end
+
+  def link_count
+    link_model.count
   end
 
 end
