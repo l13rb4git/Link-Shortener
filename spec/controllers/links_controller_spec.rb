d5e3bc4d20d26d6476d6dd49417155d86ce81ac0
macunaima <gcgamer46@gmail.com>
2020-10-08 13:32:53 -0300

    testing if the response is rendering a template

--- spec/controllers/links_controller_spec.rb
+++ spec/controllers/links_controller_spec.rb
@@ -3,16 +3,25 @@ require "rails_helper"
 
 RSpec.describe LinksController, type: :controller do
   let(:valid_attributes) {
-    { :url => "https://www.website.com/videos/first-video.mp4" }
+    { :original_url => "https://www.website.com/videos/first-video.mp4" }
   }
 
 
   describe "POST #create" do
     context "with valid params" do
       it 'creates a new Link record' do
+        request.env["HTTP_ACCEPT"] = "text/javascript"
+
         expect {
           post :create, params: { link: valid_attributes }
         }.to change(Link, :count).by(1)
+      end
+
+      it 'renders a js template' do
+        request.env["HTTP_ACCEPT"] = "text/javascript"
+
+        post :create, params: { link: valid_attributes }
+        expect(response).to render_template('create')
       end
     end
   end
