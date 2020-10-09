40703d93ba8cb4d77a7d1fedc4cbb57a2749cbba
macunaima <gcgamer46@gmail.com>
2020-10-09 10:42:53 -0300

    validating if the original_url has a host and is not blank?

--- app/models/link.rb
+++ app/models/link.rb
@@ -1,4 +1,14 @@
 class Link < ApplicationRecord
   validates_presence_of :original_url, :lookup_code
   validates_uniqueness_of :lookup_code
+  validate :original_url_format, unless: Proc.new { |link| link.original_url.blank? }
+
+
+  def original_url_format
+    uri = URI.parse(original_url)
+
+    if uri.host.nil?
+      errors.add(:original_url, "Invalid URL format")
+    end
+  end
 end
